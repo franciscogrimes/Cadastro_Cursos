2ba9e604e50f9582bd6f5e38f5560a600d7cbaf7
franciscogrimes <fgsieg1831@gmail.com>
2024-07-09 13:01:40 -0300

    Inserção de recursos e migration

--- src/config/database.config.js
+++ src/config/database.config.js
@@ -2,9 +2,14 @@ const { config } = require("dotenv");
 config();
 
 module.exports = {
-  dialect: "postgres",
-  host: "localhost",
-  port: 5432,
-  username: "postgres",
-  password: "pgData5432",
+  dialect: process.env.DB_DIALECT,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  // dialect: "postgres",
+  // host: "localhost",
+  // port: 5432,
+  // username: "postgres",
+  // password: "pgData5432",
 };
