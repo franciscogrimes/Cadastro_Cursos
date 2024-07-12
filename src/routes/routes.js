1fbc19aa021e304d1d343339a8770e8e3984dc6a
franciscogrimes <fgsieg1831@gmail.com>
2024-07-12 19:51:17 -0300

    Finalizando último exercício

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,8 +1,10 @@
 const { Router } = require("express");
 const cursosRoutes = require("./cursos.routes");
+const professoresRoutes = require("./professores.routes");
 
 const routes = new Router();
 
 routes.use("/cursos", cursosRoutes);
+routes.use("/professores", professoresRoutes);
 
 module.exports = routes;
