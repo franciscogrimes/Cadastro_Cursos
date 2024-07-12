f40080b9b35c1b423a51fcecb8400b7c4adf65ca
franciscogrimes <fgsieg1831@gmail.com>
2024-07-12 19:50:45 -0300

    Realizando as configurações dos métodos que são exigidos em todos os exercícios

--- src/controllers/CursoControllers.js
+++ src/controllers/CursoControllers.js
@@ -11,7 +11,90 @@ class CursoController {
       return response.status(500).json({
         mensagem: `Não foi possível cadastrar curso devido ao erro a seguir:\n ${error}`,
       });
-      console.log(error);
+    }
+  }
+
+  async listaCursos(request, response) {
+    try {
+      const dados = request.body;
+
+      if (!dados.id || !dados.nomeCurso || !dados.duracao) {
+        const resposta = await Curso.findAll(dados);
+        return response.status(200).json(resposta);
+      } else {
+      }
+    } catch (error) {
+      return response.status(500).json({
+        mensagem: "Não foi possível visualizar os cursos disponíveis.",
+      });
+    }
+  }
+  async listaUm(request, response) {
+    try {
+      const id = request.params.id;
+
+      const curso = await Curso.findByPk(id);
+      if (!curso) {
+        return response.status(404).json({
+          mensagem: "Nenhum curso encontrado com esse id.",
+        });
+      }
+
+      return response.status(200).json(curso);
+    } catch (error) {
+      console.error(error);
+      return response.status(500).json({
+        mensagem: "Não foi possível visualizar o curso.",
+      });
+    }
+  }
+  async atualizar(request, response) {
+    try {
+      const id = request.params.id;
+      const dados = request.body;
+
+      const curso = await Curso.findByPk(id);
+
+      if (!curso) {
+        return response.status(404).json({
+          mensagem: "Nenhum curso encontrado com esse id.",
+        });
+      }
+
+      curso.nomeCurso = dados.nomeCurso;
+      curso.duracao = dados.duracao;
+
+      await curso.save();
+
+      response.json(curso);
+    } catch (error) {
+      console.error(error);
+      return response.status(500).json({
+        mensagem: "Não foi possível visualizar o curso.",
+      });
+    }
+  }
+  async deletar(request, response) {
+    try {
+      const id = request.params.id;
+      const dados = request.body;
+
+      const curso = await Curso.findByPk(id);
+
+      if (!curso) {
+        return response.status(404).json({
+          mensagem: "Nenhum curso encontrado com esse id.",
+        });
+      }
+
+      await curso.destroy();
+
+      response.status(204).json();
+    } catch (error) {
+      console.error(error);
+      return response.status(500).json({
+        mensagem: "Não foi possível visualizar o curso.",
+      });
     }
   }
 }
--- src/routes/cursos.routes.js
+++ src/routes/cursos.routes.js
@@ -4,5 +4,9 @@ const CursoControllers = require("../controllers/CursoControllers");
 const cursosRoutes = new Router();
 
 cursosRoutes.post("/", CursoControllers.criarCurso);
+cursosRoutes.get("/", CursoControllers.listaCursos);
+cursosRoutes.get("/:id", CursoControllers.listaUm);
+cursosRoutes.put("/:id", CursoControllers.atualizar);
+cursosRoutes.delete("/:id", CursoControllers.deletar);
 
 module.exports = cursosRoutes;
